f5f04f5f5f0135c90705d8ac995e4b954e3fb6fb
Joshua Kuiros <joshkuiros@gmail.com>
2013-10-21 17:26:04 -0400

    core data model constructed

---
+++ PennStateSearch/PennStateSearch/MyDataManager.h
@@ -0,0 +1,13 @@
+//
+//  MyDataManager.h
+//  PennStateSearch
+//
+//  Created by Joshua Kuiros on 10/21/13.
+//  Copyright (c) 2013 Joshua Kuiros. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyDataManager : NSObject
+
+@end
---
+++ PennStateSearch/PennStateSearch/MyDataManager.m
@@ -0,0 +1,13 @@
+//
+//  MyDataManager.m
+//  PennStateSearch
+//
+//  Created by Joshua Kuiros on 10/21/13.
+//  Copyright (c) 2013 Joshua Kuiros. All rights reserved.
+//
+
+#import "MyDataManager.h"
+
+@implementation MyDataManager
+
+@end
